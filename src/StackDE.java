30ced349225828881025fce14e11f9b4e537e875
valyas01 <johana.ve96@gmail.com>
2015-08-14 13:45:43 -0600

    Update StackDE.java

--- src/StackDE.java
+++ src/StackDE.java
@@ -1,40 +1,53 @@
 
 public class StackDE<E> implements ADTStack<E> {
-
+	
+	protected DoblementeEnlazada<E> pila;
+	
 	@Override
 	public void empty() {
+		int size = pila.size();
+		for (int i = 0; i <= size; i++){
+			pila.removeFirst();
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
+		
+		if(pila.size() == 0)	return true;
+		else return false;
+		//yas TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public void push(E x) {
+	public void push(E dat) {
 		// TODO Auto-generated method stub
+		pila.addFirst(dat);
 		
 	}
 
 	@Override
 	public E pop() throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return pila.removeFirst();
 	}
 
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		return pila.size();
 	}
 
 	@Override
 	public E peek() throws Exception {
 		// TODO Auto-generated method stub
+		return pila.getFirst();
+		// TODO Auto-generated method stub
 		return null;
 	}
+		
+
 
 }
